4229127c47188a8636b979fd775143b0f44c31d5
augusto f. hack <hack.augusto@gmail.com>
2013-07-02 00:05:19 -0300

    fato censo

--- dados/filtra_csv.sh
+++ dados/filtra_csv.sh
@@ -1,4 +1,4 @@
-for i in *csv;
+for i in *csv; do
     # retira cabecalho
     # ignora linhas que nao interessam
     # remove caracters de formatacao dos numeros
@@ -6,3 +6,6 @@ for i in *csv;
         awk 'BEGIN {FS="\t"; OFS="\t"}; { print $2, $11, $12 }' |  
         tr -d ,. |  
         awk 'BEGIN {FS="\t"; OFS="\t"}; (NR % 6 == 1) { nome = $1 }; (NR % 6 == 0){ print nome, $2 + $3 }' > ${i}_out;
+
+    awk -f agrupa_estado.awk ${i}_out | sponge ${i}_out;
+done;
